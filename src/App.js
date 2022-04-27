bc408249a1db4438f6243dc935a796a26fde9e49
codincowboyx <codincowboy@rebelsloths.com>
2022-04-27 09:04:57 -0700

    Adding color and size changing capability

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import rawrlex from './rawrlex.png';
 import Input from '@mui/material/Input';
 import './App.css';
 import Button from '@mui/material/Button';
+import ColorPicker from 'material-ui-color-picker';
+import { Slider } from '@mui/material';
 
 function App() {
   const [dinoId, setDinoId] = useState(1);
+  const [color, setColor] = useState("#000000");
+  const [size, setSize] = useState(100);
 
   return (
     <div className="App">
@@ -20,17 +24,42 @@ function App() {
           onChange={(e) => {
             setDinoId(e.target.value);
           }}/>
+        <div className='Color-picker-container'>
+          <h3 className='Input-label'>Choose Color:</h3>
+          <ColorPicker value={color} variant="outlined" TextFieldProps={{ value: color }} onChange={colorVal => {
+              setColor(colorVal);
+            }} />
+        </div>
+        <div className='Size-slider-container'>
+          <h3 className='Input-label'>Choose Face Size:</h3>
+          <Slider value={size} onChange={(event, sizeVal) => {
+              setSize(sizeVal);
+            }} />
+        </div>
         <Button onClick={() => { window.print() }} variant="contained" sx={{ marginTop: "20px" }}>Print</Button>
+
       </div>
+      
       <div className='Rawrlex-container'>
-        <div className='dashed-container'>
-          <div className='solid-container'>
-            <div className='watch-face'>
-              <img src={`https://raw.githubusercontent.com/tinydinosnft/tinydinosassets/main/images/dinos/1600x1600/transparent/${dinoId}.png`} className="Rawrlex-dino-img" alt="dino" />
-            </div>
+        <div className='watch-face' style={{
+          border: `${size * 0.2}px solid ${color}`,
+          width: `${size * 3.5}px`,
+          height: `${size * 3.5}px`
+        }}>
+          <img style={{
+            width: `${size * 2.5}px`,
+            height: `${size * 2.5}px`
+          }} src={`https://raw.githubusercontent.com/tinydinosnft/tinydinosassets/main/images/dinos/1600x1600/transparent/${dinoId}.png`} className="Rawrlex-dino-img" alt="dino" />
+        </div>
+        <div className='dashed-container' style={{
+              border: `2px dotted ${color}`
+            }}>
+          <div className='solid-container' style={{
+              border: `2px solid ${color}`
+            }}>
+            
           </div>
         </div>
-        
       </div>
       
     </div>
